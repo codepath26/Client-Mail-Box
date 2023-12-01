a6ee40de32b49bece3007d374235414bfc3cee3b
parth thakor <parththakor2610@gmail.com>
2023-12-01 09:19:06 +0530

    adding the delete mail functionality

--- src/Components/Inbox/Inbox.js
+++ src/Components/Inbox/Inbox.js
@@ -1,51 +1,76 @@
-import React, { useEffect } from 'react'
-import { Button, Container } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchInbox, setReadMail } from '../../Store/EmailSlice';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Button, Container } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteMail, fetchInbox, setReadMail } from "../../Store/EmailSlice";
+import { useNavigate } from "react-router-dom";
 
 function Inbox() {
-  const emails = useSelector(state=>state.emails);
-  const email = localStorage.getItem('userEmail');
+  const emails = useSelector((state) => state.emails);
+  const email = localStorage.getItem("userEmail");
   // console.log("find from the local storage" , email);
   const inboxMails = emails.inboxEmails;
   const dispatch = useDispatch();
-  const  navigate = useNavigate();
-  useEffect(()=>{
+  const navigate = useNavigate();
+  useEffect(() => {
     // console.log("at Inbox useeffect bro")
     // console.log(email)
     dispatch(fetchInbox(email));
-  },[email ,dispatch]);
-  const getDetails = (email)=>{
+  }, [email, dispatch]);
+  const getDetails = (email) => {
     // const reademailsId = localStorage.getItem('readEmails');
     // localStorage.setItem('readEmails' ,[...reademailsId,id]);
-   dispatch(setReadMail(email.id));
+    dispatch(setReadMail(email.id));
     navigate(`/maildetails/${email.id}`);
+  };
+  const removeMail = (id)=>{
+    console.log("removeMailis Called with id" , id);
+    dispatch(deleteMail(id));
   }
   return (
-  <Container>
-    <div className="vh-100">
-      <div className="d-flex justify-content-between p-2">
-        <span>Welcome To Your MailBox</span>
-        <Button variant='primary' onClick={()=>navigate("/main")}>Back</Button>
+    <Container>
+      <div className="vh-100">
+        <div className="d-flex justify-content-between p-2">
+          <span>Welcome To Your MailBox</span>
+          <Button variant="primary" onClick={() => navigate("/main")}>
+            Back
+          </Button>
+        </div>
+        <div className="border border-danger">
+          <ul>
+            {inboxMails.map((email) => {
+              return (
+                <div
+                  key={email.id}
+                  onClick={() => getDetails(email)}
+                  className="text-primary  cursor-pointer border mt-2 p-1 d-flex justify-content-between align-items-center"
+                >
+                  <div>
+                    {email.readMail ? (
+                      ""
+                    ) : (
+                      <span className="ms-1 me-2 position-relative ">
+                        <span className="position-absolute top-50 start-50 translate-middle p-1 bg-danger border border-light rounded-circle"></span>
+                      </span>
+                    )}
+                    <span className="me-2">Sender : {email.sender}</span>
+                    <span>Subject : {email.subject}</span>{" "}
+                  </div>
+                  <div>
+                  <button onClick={(e)=>{
+                    e.stopPropagation();
+                    removeMail(email.id)
+                    }} className="btn btn-sm btn-danger float-end me-5">
+                    Remove
+                  </button>
+                  </div>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
       </div>
-      <div className="border border-danger">
-       <ul>
-        {
-          inboxMails.map((email)=>{
-           return <div key={email.id} onClick={()=>getDetails(email)} className='text-primary  cursor-pointer border mt-2 p-1 '>
-            {email.readMail ? "" : <span className='ms-1 me-2 position-relative '>
-            <span class="position-absolute top-50 start-50 translate-middle p-1 bg-danger border border-light rounded-circle"></span></span>}
-            <span>Sender : {email.sender || "no sender"}</span>
-            <span>Subject : {email.subject}</span>
-            </div>
-          })
-        }
-       </ul>
-      </div>
-    </div>
-  </Container>
-  )
+    </Container>
+  );
 }
 
 export default Inbox;
--- src/Store/EmailSlice.js
+++ src/Store/EmailSlice.js
@@ -6,34 +6,37 @@ const inititalemailState = {
   inboxEmails: [],
   error: null,
   status: "idle",
-  unReadEmails : 0
+  unReadEmails: 0,
 };
 
-export const fetchMail = createAsyncThunk("emails/fetchMail", async (userEmail) => {
-  try {
-    const response = await axios.get(
-      `${process.env.REACT_APP_FIREBASE_URL}email.json`
-    );
-    if (response.data) {
-      const emailArray = Object.keys(response.data).map((key) => {
-        return {
-          id: key,
-          ...response.data[key],
-        };
-      });
-      const senderEmail = emailArray.filter(email => {
-        return email.sender === userEmail ;
-      })
-      // console.log("sender email" ,senderEmail)
-      return senderEmail;
-    } else {
-      return [];
+export const fetchMail = createAsyncThunk(
+  "emails/fetchMail",
+  async (userEmail) => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_FIREBASE_URL}email.json`
+      );
+      if (response.data) {
+        const emailArray = Object.keys(response.data).map((key) => {
+          return {
+            id: key,
+            ...response.data[key],
+          };
+        });
+        const senderEmail = emailArray.filter((email) => {
+          return email.sender === userEmail;
+        });
+        // console.log("sender email" ,senderEmail)
+        return senderEmail;
+      } else {
+        return [];
+      }
+    } catch (err) {
+      console.log(err);
+      return err;
     }
-  } catch (err) {
-    console.log(err);
-    return err;
   }
-});
+);
 
 export const fetchInbox = createAsyncThunk("fetchInbox", async (emailId) => {
   try {
@@ -61,9 +64,22 @@ export const setReadMail = createAsyncThunk("setReadMail", async (id) => {
   try {
     const response = await axios.patch(
       `${process.env.REACT_APP_FIREBASE_URL}email/${id}.json`,
-      {readMail : true}
+      { readMail: true }
     );
-    console.log("from setreammails",response);
+    console.log("from setreammails", response);
+  } catch (err) {
+    console.log(err);
+  }
+});
+export const deleteMail = createAsyncThunk("deleteMail", async (id) => {
+  try {
+    console.log("starting of delete Mail");
+    const response = await axios.delete(
+      `${process.env.REACT_APP_FIREBASE_URL}email/${id}.json`
+    );
+    console.log(response.data, "from delteMail");
+    console.log("ending of delete mail", id);
+    return id;
   } catch (err) {
     console.log(err);
   }
@@ -100,19 +116,32 @@ export const emailSlice = createSlice({
     });
     builder.addCase(fetchInbox.fulfilled, (state, action) => {
       state.status = "successed";
-      state.inboxEmails = action.payload; 
-      state.unReadEmails = action.payload.reduce((acc ,total)=> {
+      state.inboxEmails = action.payload;
+      state.unReadEmails = action.payload.reduce((acc, total) => {
         return acc + (total.readMail === false);
-      } ,0);
-      console.log(state.unReadEmails , "unread message");
+      }, 0);
+      console.log(state.unReadEmails, "unread message");
     });
     builder.addCase(fetchInbox.pending, (state, action) => {
       state.status = "pending";
       // state.error = action.error.message;
+    });
+    builder.addCase(deleteMail.fulfilled, (state, action) => {
+      console.log("id", action.payload);
+      const oldEmails = [...state.inboxEmails];
+      console.log("odl mails", oldEmails);
+      const newEmails = oldEmails.filter((email) => {
+        console.log(email.id, "<==>", action.payload);
+        return email.id !== action.payload;
+      });
+      state.inboxEmails = newEmails;
+      console.log("new emails", state.emails);
+    });
+    builder.addCase(deleteMail.rejected, (state, action) => {
+      console.log("rejected", action.payload);
     });
   },
 });
 
 export const emailActions = emailSlice.actions;
 export default emailSlice.reducer;
-
